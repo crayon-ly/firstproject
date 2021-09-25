6e8b01ba0285f8553e88ac8242e9de15ba1ba38f
lyy <790543071@qq.com>
2021-09-25 18:27:45 +0800

    5

--- Demo/src/main/java/com/example/lyy/demo/A.java
+++ Demo/src/main/java/com/example/lyy/demo/A.java
@@ -2,6 +2,6 @@ package com.example.lyy.demo;
 
 public class A {
     public static void main(String[] args) {
-        System.out.println("功能2开发完毕");
+        System.out.println("功能2开发完毕git");
     }
 }
