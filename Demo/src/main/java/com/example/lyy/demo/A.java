c5b33ae118cae7d505dd160cdd64c3c4642e6bd9
lyy <790543071@qq.com>
2021-10-31 21:05:40 +0800

    develop

--- Demo/src/main/java/com/example/lyy/demo/A.java
+++ Demo/src/main/java/com/example/lyy/demo/A.java
@@ -2,9 +2,12 @@ package com.example.lyy.demo;
 
 public class A {
     public static void main(String[] args) {
-        System.out.println("功能2开发完毕git");
-        System.out.println("功能3开发完毕git");
-        System.out.println("功能4开发完毕git");
+        System.out.println("功能2");
+        System.out.println("功能3fhdfff");
+        System.out.println("功能4开发完毕");
+        System.out.println("功能4开发完毕");
+        System.out.println("功能4开发完毕");
+        System.out.println("功能4开发完毕");
     }
 
 }
