e4d99c95f43e8713e65cd13a6012170cfbfc5e5f
lyy <790543071@qq.com>
2021-10-31 21:20:56 +0800

    Revert "1"
    This reverts commit 5e37f6290e0c124efc31169defb9f258a6e4db75.

---
+++ Demo/src/main/java/com/example/lyy/demo/A.java
@@ -0,0 +1,10 @@
+package com.example.lyy.demo;
+
+public class A {
+    public static void main(String[] args) {
+        System.out.println("功能1开发完毕git");
+        System.out.println("功能2开发完毕git");
+        System.out.println("功能5开发完毕git");
+    }
+
+}
