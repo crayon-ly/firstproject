0949c79046721bf6cd3d94ce609763128529aa1d
lyy <790543071@qq.com>
2021-10-31 21:02:39 +0800

    功能模块

--- Demo/src/main/java/com/example/lyy/demo/A.java
+++ Demo/src/main/java/com/example/lyy/demo/A.java
@@ -2,9 +2,9 @@ package com.example.lyy.demo;
 
 public class A {
     public static void main(String[] args) {
+        System.out.println("功能1开发完毕git");
         System.out.println("功能2开发完毕git");
-        System.out.println("功能3开发完毕git");
-        System.out.println("功能4开发完毕git");
+        System.out.println("功能5开发完毕git");
     }
 
 }
