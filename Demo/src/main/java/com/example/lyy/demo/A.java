de7f94a7f93022a61c6954696afbb9690b9f714c
lyy <790543071@qq.com>
2021-10-31 20:41:20 +0800

    功能3开发完毕

--- Demo/src/main/java/com/example/lyy/demo/A.java
+++ Demo/src/main/java/com/example/lyy/demo/A.java
@@ -3,5 +3,7 @@ package com.example.lyy.demo;
 public class A {
     public static void main(String[] args) {
         System.out.println("功能2开发完毕git");
+        System.out.println("功能3开发完毕git");
     }
+
 }
